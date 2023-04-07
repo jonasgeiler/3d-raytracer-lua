963627783d42039434d95bd4a46624f6e40240d0
Jonas Geiler <contact@skayo.dev>
2023-04-07 23:55:32 +0200

    chore: removed old JS code

--- src/color.ts
+++
@@ -1,6 +0,0 @@
-import type { FileHandle } from 'node:fs/promises';
-import type { Color } from './vec3.js'
-
-export async function writeColor(fh: FileHandle, pixelColor: Color) {
-	await fh.write(`${Math.trunc(255.999 * pixelColor.x)} ${Math.trunc(255.999 * pixelColor.y)} ${Math.trunc(255.999 * pixelColor.z)}\n`);
-}
