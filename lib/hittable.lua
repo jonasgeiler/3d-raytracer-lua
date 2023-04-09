c3c5248aa5ba4356f54a6866ba610031884fca34
Jonas Geiler <skayo.dev@gmail.com>
2023-04-10 01:08:08 +0200

    feat: improved hittable class

--- lib/hittable.lua
+++ lib/hittable.lua
@@ -2,4 +2,8 @@ local class = require('lib.middleclass')
 
 local hittable = class('hittable')
 
+function hittable:hit()
+	error('Not hit method defined for hittable object')
+end
+
 return hittable
